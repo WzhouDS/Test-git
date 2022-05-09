8bfeb44b601a423ca4ca274127956b2f16d589b4
Wzhou <1156913092@qq.com>
2022-05-09 11:33:23 +0800

    Second commit

--- src/main/java/com/Wzhou/FitrstTest.java
+++ src/main/java/com/Wzhou/FitrstTest.java
@@ -3,5 +3,6 @@ package com.Wzhou;
 public class FitrstTest {
     public static void main(String[] args) {
         System.out.println("test one");
+        System.out.println("test two");
     }
 }
